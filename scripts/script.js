b5dc595301176f0a8996665a349a18f1c345aa54
The Phuc <thephuc1601@gmail.com>
2017-03-12 14:37:36 +0700

    fix some content

--- scripts/script.js
+++ scripts/script.js
@@ -17,12 +17,6 @@ function loadScript() {
     css.href = chrome.extension.getURL('css/jquery-ui.min.css');
     document.head.appendChild(css);
 
-    var css = document.createElement("link");
-    css.type = "text/css";
-    css.rel  = 'stylesheet';
-    css.href = chrome.extension.getURL('css/iframe.css');
-    document.head.appendChild(css);
-
     var div = document.createElement("div");
     div.id = "Youtoo-loaded";
     document.body.appendChild(div);
@@ -35,8 +29,10 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
         document.getElementById('Youtoo').remove();
 
     var div = document.createElement('div');
-    div.id = 'Youtoo';
-    div.innerHTML = '<iframe width="360" height="215" src="https://www.youtube.com/embed/'+message+'" frameborder="0"></iframe>';
+    div.id = "Youtoo";
+    var content = "<style>#Youtoo{cursor: -webkit-grabbing;width:360px;height:210px;background-image:url("+chrome.extension.getURL('img/bg.jpg')+");z-index:1000;position:fixed;top:30%;left:35%;border-radius:2px;padding:25px 0 0}</style>"
+    content += '<iframe width="360" height="215" src="https://www.youtube.com/embed/'+message+'" frameborder="0"></iframe>';
+    div.innerHTML = content;
     document.body.appendChild(div);
 
     $( "#Youtoo" ).draggable();
